82560933a04f3d4595c06aed48d4097c3b109724
Jared Lunde <jared.lunde@gmail.com>
2019-08-09 18:44:07 -0600

    Renames createdAt parser in reducers

--- packages/www/src/state/reducers.js
+++ packages/www/src/state/reducers.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 import * as actions from './actions'
 
 
-const parseCreatedAt = data => ({
+const assignWithCreatedAt = data => ({
   ...data,
   createdAt: isNaN(data.createdAt) ? data.createdAt : new Date(data.createdAt)
 })
@@ -10,11 +10,11 @@ const parseCreatedAt = data => ({
 export const company = (state = {}, action) => {
   switch (action.type) {
     case actions.ADD_COMPANY:
-      return {...parseCreatedAt(action.company), status: action.status}
+      return {...assignWithCreatedAt(action.company), status: action.status}
     case actions.GET_COMPANY:
       return {status: action.status}
     case actions.RECEIVE_COMPANY:
-      return {...parseCreatedAt(action.company), status: action.status}
+      return {...assignWithCreatedAt(action.company), status: action.status}
     default:
       return state
   }
@@ -25,7 +25,7 @@ export const shareholders = (state = {}, action) => {
     case actions.ADD_SHAREHOLDER:
       if (action.status === actions.Api.DONE)
         return {
-           members: [...state.members, parseCreatedAt(action.shareholder)],
+           members: [...state.members, assignWithCreatedAt(action.shareholder)],
            status: action.status
         }
       return {...state, status: action.status}
@@ -36,7 +36,7 @@ export const shareholders = (state = {}, action) => {
         for (let i = 0; i < members.length; i++) {
           const member = members[i]
           if (member.id === action.shareholder.id)
-            members[i] = {...member, ...parseCreatedAt(action.shareholder)}
+            members[i] = {...member, ...assignWithCreatedAt(action.shareholder)}
         }
 
         return {members, status: action.status}
@@ -56,7 +56,7 @@ export const shareholders = (state = {}, action) => {
       return {status: action.status}
 
     case actions.RECEIVE_SHAREHOLDERS:
-      return {members: action.shareholders.map(parseCreatedAt), status: action.status}
+      return {members: action.shareholders.map(assignWithCreatedAt), status: action.status}
 
     default:
       return state
@@ -68,7 +68,7 @@ export const shares = (state = [], action) => {
     case actions.ADD_SHARE:
       if (action.status === actions.Api.DONE)
         return {
-          members: [...state.members, parseCreatedAt(action.share)],
+          members: [...state.members, assignWithCreatedAt(action.share)],
           status: action.status
         }
       return {...state, status: action.status}
@@ -79,7 +79,7 @@ export const shares = (state = [], action) => {
         for (let i = 0; i < members.length; i++) {
           const member = members[i]
           if (member.id === action.share.id)
-            members[i] = {...member, ...parseCreatedAt(action.share)}
+            members[i] = {...member, ...assignWithCreatedAt(action.share)}
         }
 
         return {members, status: action.status}
@@ -99,7 +99,7 @@ export const shares = (state = [], action) => {
       return {status: action.status}
 
     case actions.RECEIVE_SHARES:
-      return {members: action.shares.map(parseCreatedAt), status: action.status}
+      return {members: action.shares.map(assignWithCreatedAt), status: action.status}
 
     default:
       return state
